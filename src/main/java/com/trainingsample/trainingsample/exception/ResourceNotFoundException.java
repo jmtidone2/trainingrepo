79a6f3f628ed09b7bf0a2cd6c2da0a57a7a977f5
jmtid <jmtidone@gmail.com>
2021-01-31 15:45:08 -0300

    commit from PROD-102246, number 2

--- src/main/java/com/trainingsample/trainingsample/exception/ResourceNotFoundException.java
+++ src/main/java/com/trainingsample/trainingsample/exception/ResourceNotFoundException.java
@@ -2,6 +2,6 @@ package com.trainingsample.trainingsample.exception;
 
 public class ResourceNotFoundException extends Exception{
     public ResourceNotFoundException(String message) {
-        super  (message);
+        super  (message); //Nothing
     }
 }
